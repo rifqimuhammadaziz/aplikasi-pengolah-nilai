24cc0607411781517570c6c7f12c103c70705860
Rifqi Muhammad Aziz <IDS.RIFQIM@xl.co.id>
2023-10-25 09:40:45 +0700

    add third menu

---
+++ src/main/java/com/rifqimuhammadaziz/ThirdMenu.java
@@ -0,0 +1,28 @@
+package com.rifqimuhammadaziz;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static com.rifqimuhammadaziz.Utility.*;
+
+public class ThirdMenu {
+
+    public static void printAllFile(Scanner scanner) {
+        boolean isHomePage = false;
+
+        while (!isHomePage) {
+            showHeader();
+
+            ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+            executorService.submit(Utility::printModus);
+            executorService.submit(Utility::printModusMedianMean);
+
+            executorService.shutdown();
+            System.out.println("-- FILE BERHASIL GENERATE DI");
+
+            isHomePage = isHomePage(scanner, isHomePage);
+        }
+    }
+}
